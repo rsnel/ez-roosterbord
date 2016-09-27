92edf8cd2feaed1908d09f4a57824c6580c68062
Rik Snel <rik@snel.it>
2016-09-27 06:56:29 +0200

    ruil verticaal door klikken en/of slepen

--- raw.php
+++ raw.php
@@ -5,9 +5,15 @@ $entity_ids = array();
 
 if (isset($_GET['file_id_basis']) && $_GET['file_id_basis']) $file_id_basis = $_GET['file_id_basis'];
 else fatal_error("parameter file_id_basis is required");
+$file_basis = mdb2_single_assoc("SELECT * FROM files WHERE file_id = %i", $file_id_basis);
+
+if (!$file_basis) fatal_error("invalid file_id_basis");
 
 if (isset($_GET['file_id_wijz']) && $_GET['file_id_wijz']) $file_id_wijz = $_GET['file_id_wijz'];
 else $file_id_wijz = 0;
+
+$file_wijz = mdb2_single_assoc("SELECT * FROM files WHERE file_id = %i", $file_id_basis);
+if (!$file_wijz) fatal_error("invalid file_id_basis");
 
 if (isset($_GET['dag']) && is_array($_GET['dag'])) $dispdagen = $_GET['dag'];
 else $dispdagen = array ( 'ma', 'di', 'wo', 'do', 'vr');
@@ -24,6 +30,14 @@ if (isset($_GET['nq'])) {
 	$entities = array_filter($entities, 'checknq');
 }
 $entities = array_unique($entities);
+
+
+function escape_and_quote($entity) {
+	return "'".mdb2()->escape($entity, true)."'";
+}
+
+$escaped_entities = array_map('escape_and_quote', $entities);
+$escaped_entities = implode(',', $escaped_entities);
 $searchbox = implode(',', $entities);
 $tail = '';
 for ($i = 1; $i <= 5; $i++) if (in_array($dagen[$i-1], $dispdagen)) $tail .= "&amp;dag[]={$dagen[$i-1]}";
@@ -31,19 +45,43 @@ $tail .= '&amp;file_id_basis='.$file_id_basis.'&amp;file_id_wijz='.$file_id_wijz
 $head = '<a href="raw.php?q='.$searchbox;
 
 foreach ($entities as $entity) {
-	$row = mdb2_single_array("SELECT entity_id, entity_type FROM entities WHERE entity_name = '%q' AND 
+	$row = mdb2_single_array("SELECT entity_id, entity_type, entity_name FROM entities WHERE entity_name = '%q' AND 
 ( entity_type = %i OR entity_type = %i )", trim($entity), DOCENT, LOKAAL);
 	if ($row) {
 		$entity_ids[] = $row;
 	}
 }
+function compare_entities($a, $b) {
+	if ($a[1] < $b[1]) return 1;
+	if ($a[1] > $b[1]) return -1;
+	return strcmp($a[2], $b[2]);
+}
+usort($entity_ids, 'compare_entities');
+$enable_edit = get_enable_edit();
 $select_doc = array();
 $select_lok = array();
 for ($i = 1; $i <= 5; $i++) {
 	if (!in_array($dagen[$i-1], $dispdagen)) continue;
 	for ($j = 1; $j <= 9; $j++) {
-		$select_lok[] = "IFNULL(GROUP_CONCAT(IF(dag = $i AND uur = $j, IF(lokalen = '', '?', CONCAT('$head,', lokalen, '$tail', lokalen, '</a>')), NULL) SEPARATOR '<br>'), '-') {$dagen[$i-1]}$j";
-		$select_doc[] = "IFNULL(GROUP_CONCAT(IF(dag = $i AND uur = $j, IF(LENGTH(docenten) > 6, '****', CONCAT('$head,', docenten, '$tail', docenten, '</a>')), NULL) SEPARATOR '<br>'), '-') {$dagen[$i-1]}$j";
+		$uur = "$i-$j";
+		if ($enable_edit) $colname = "`<input type=\"checkbox\" name=\"lesuur[]\" value=\"$i-$j\">{$dagen[$i-1]}$j`";
+		else $colname = "{$dagen[$i-1]}$j";
+		$select_lok[] = "IFNULL(GROUP_CONCAT(IF(dag = $i AND uur = $j, IF(lokalen = '', '?', IF(LENGTH(lokalen) > 6, '****', IF(lokalen IN ( $escaped_entities), lokalen, CONCAT('$head,', lokalen, '$tail', lokalen, '</a>')))), NULL) SEPARATOR '<br>'), '-') $colname";
+		$select_doc[] = <<<EOQ
+IFNULL(
+	GROUP_CONCAT(
+		IF(dag = $i AND uur = $j,
+			IF(LENGTH(docenten) > 6, '****',
+				IF (docenten IN ( $escaped_entities),
+					CONCAT('<div class="dd$uur">', docenten, '</div>'),
+					CONCAT('$head,', docenten, '$tail', docenten, '</a>')
+				)
+			),
+			NULL
+		) SEPARATOR '<br>'),
+	'<div class="dd$uur">-</div>'
+) $colname
+EOQ;
 	}
 }
 $select_lok = implode(",\n", $select_lok);
@@ -54,11 +92,16 @@ $query = array();
 
 foreach ($entity_ids as $entity_id) {
 	$subquery = rquery($entity_id[0], $entity_id[0], $file_id_basis, $file_id_wijz, 'LEFT ');
-	if ($entity_id[1] == LOKAAL) $select = $select_doc;
-	else $select = $select_lok;
+	if ($entity_id[1] == LOKAAL && $enable_edit) {
+		$select = $select_doc;
+		$dolo = "'<input type=\"checkbox\" name=\"lokaal[]\" value=\"{$entity_id[2]}\"> $head&amp;nq={$entity_id[2]}$tail{$entity_id[2]}</a>' dolo";
+	} else {
+		$select = $select_lok;
+		$dolo = "'$head&amp;nq={$entity_id[2]}$tail{$entity_id[2]}</a>' dolo";
+	}
 
 	$query[] = <<<EOQ
-SELECT CONCAT('$head', '&amp;nq=', (SELECT entity_name FROM entities WHERE entity_id = {$entity_id[0]} ), '$tail', (SELECT entity_name FROM entities WHERE entity_id = {$entity_id[0]} ), '</a>') dolo, $select
+SELECT $dolo, $select
  FROM (
 	$subquery
 ) AS base
@@ -74,6 +117,46 @@ $result = mdb2_query($query);
 ?>
 <!DOCTYPE html!>
 <head>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.10.4.custom.min.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+function initdd(i, j) {
+	var spec = i + '-' + j;
+	$('.dd' + spec).draggable({
+		containment: "#tablecontainer",
+		axis: "y",
+		revert: 'invalid',
+		opacity: 0.7, helper: "clone"
+	});
+	$('.dd' + spec).droppable({
+		accept: '.dd' + spec,
+		drop: function( event, ui ) {
+			$('input', $(this).parent().parent()).attr('checked', true);
+			$('input', ui.draggable.parent().parent()).attr('checked', true);
+			$("input[value='" + spec + "']").attr('checked', true);
+			$("#ruil").submit();
+		},
+		activate: function() {
+			$(this).addClass('active');
+		},
+		deactivate: function() {
+			$(this).removeClass('active');
+		}
+	});
+}
+
+$(function(){
+<? if ($enable_edit) { ?>
+	for (var i = 1; i <= 5; i++) {
+		for (var j = 1; j <= 9; j++) {
+			initdd(i, j);
+		}
+	}
+<? } ?>
+});
+//]]>
+</script>
 <style>
 a:link {
         text-decoration: none; color: blue;
@@ -83,6 +166,9 @@ a:visited {
 }
 a:hover {
         text-decoration: underline;
+}
+.active {
+	background-color: #FFFF99;
 }
 /*td {
 	text-align: center;
@@ -103,7 +189,25 @@ docenten,lokalen<input size="60" type="text" name="q" value="<? echo($searchbox)
 <input type="submit" value="Zoek">
 </form>
 <?
+if ($enable_edit) { ?>
+<form id="ruil" action="do_ruil_verticaal.php" method="POST" accept-charset="UTF-8">
+<div id="tablecontainer">
+<? } 
 mdb2_res_table($result);
+?></div><?
+if ($enable_edit) {
+	if (in_array('ma', $dispdagen)) { ?><input type="hidden" name="dag[]" value="ma"><? } 
+	if (in_array('di', $dispdagen)) { ?><input type="hidden" name="dag[]" value="di"><? } 
+	if (in_array('wo', $dispdagen)) { ?><input type="hidden" name="dag[]" value="wo"><? } 
+	if (in_array('do', $dispdagen)) { ?><input type="hidden" name="dag[]" value="do"><? } 
+	if (in_array('vr', $dispdagen)) { ?><input type="hidden" name="dag[]" value="vr"><? } 
+?>
+<input type="hidden" name="q" value="<? echo($searchbox); ?>">
+Selecteer precies twee rijen en een aantal kolommen en <input type="submit" value="Ruil verticaal"> 
+Je kunt ook een docent slepen naar een andere docent of een lege plek om van lokaal te ruilen. Je kunt alleen slepen met en slepen naar docenten van wie het rooster zichtbaar is.
+<input type="hidden" name="file_id_basis" value="<? echo($file_id_basis); ?>">
+<input type="hidden" name="file_id_wijz" value="<? echo($file_id_wijz); ?>">
+<? } 
 ?>
 </body>
 </html>

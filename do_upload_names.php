33b37caa665feafb70ed9e6a909548c12e4f583a
Rik Snel <rik@snel.it>
2019-07-19 23:49:41 +0200

    fix trailing \n, rename error msgs

--- do_upload_names.php
+++ do_upload_names.php
@@ -17,25 +17,30 @@ echo($filename);
 
 $lines = file($_FILES['uploadedFile']['tmp_name']);
 
-$legenda = explode("\t", array_shift($lines));
+/* use trim() to remove trailing \n */
+$legenda = explode("\t", trim(array_shift($lines)));
 print_r($legenda);
 
 if (($llnr_index = array_search('Object', $legenda)) === FALSE) fatal_error("kolom Object niet gevonden in geupload bestand");
 
 if (($voornaam_index = array_search('Voornaam', $legenda)) === FALSE) fatal_error("kolom Voornaam niet gevonden in geupload bestand");
 
-if (($tussenvoegsels_index = array_search('Tussenvoegsels', $legenda)) === FALSE) fatal_error("kolom Voornaam niet gevonden in geupload bestand");
+if (($tussenvoegsels_index = array_search('Tussenvoegsels', $legenda)) === FALSE) fatal_error("kolom Tussenvoegsels niet gevonden in geupload bestand");
 
-if (($achternaam_index = array_search('Achternaam', $legenda)) === FALSE) fatal_error("kolom Voornaam niet gevonden in geupload bestand");
+if (($achternaam_index = array_search('Achternaam', $legenda)) === FALSE) fatal_error("kolom Achternaam niet gevonden in geupload bestand");
 
 echo("llnr_index=$llnr_index\n");
 
 foreach ($lines as $line) {
-	$columns = explode("\t", $line);
+	/* user trim() to remove trailing \n */
+	$columns = explode("\t", trim($line));
 	$llnr = $columns[$llnr_index];
 	$voornaam = fix_charset_whitespace($columns[$voornaam_index]);
 	$tussenvoegsels = fix_charset_whitespace($columns[$tussenvoegsels_index]);
 	$achternaam = fix_charset_whitespace($columns[$achternaam_index]);
+	// a broken export contained Tussenvoegsels after Achternaam
+	//$achternaam2 = explode(',', $achternaam);
+	//$achternaam = $achternaam2[0];
 	if ($tussenvoegsels == '') $name = $voornaam.' '.$achternaam;
 	else $name = $voornaam.' '.$tussenvoegsels.' '.$achternaam;
 	echo($llnr.' '.$name."\n");
@@ -43,5 +48,6 @@ foreach ($lines as $line) {
 	echo("entity_id=$entity_id\n");
 	mdb2_exec("UPDATE names SET name = '%q', firstname = '%q', prefix = '%q', surname = '%q' WHERE entity_id = '%q'", $name, $voornaam, $tussenvoegsels, $achternaam, $entity_id);
 }
+
 ?>
 
